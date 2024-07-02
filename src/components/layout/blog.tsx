623149770aaedc6603c971924b03a4bcff6c2d97
Mathias Everson <mathias@mathiaseverson.com>
2024-07-02 20:17:29 +1000

    Layout Components and scaffold

---
+++ src/components/layout/blog.tsx
@@ -0,0 +1,71 @@
+const featuredPost = {
+    id: 1,
+    title: 'We’re incredibly proud to announce we have secured $75m in Series B',
+    href: '#',
+    description:
+        'Libero neque aenean tincidunt nec consequat tempor. Viverra odio id velit adipiscing id. Nisi vestibulum orci eget bibendum dictum. Velit viverra posuere vulputate volutpat nunc. Nunc netus sit faucibus.',
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16'
+}
+const posts = [
+    {
+        id: 2,
+        title: 'Boost your conversion rate',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel iusto corrupti dicta laboris incididunt.',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-16',
+
+    },
+    // More posts...
+]
+
+export default function Blog() {
+    return (
+        <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-12 px-6 sm:gap-y-16 lg:grid-cols-2 lg:px-8">
+                <article className="mx-auto w-full max-w-2xl lg:mx-0 lg:max-w-lg">
+                    <time dateTime={featuredPost.datetime} className="block text-sm leading-6 text-slate-600">
+                        {featuredPost.date}
+                    </time>
+                    <h2 id="featured-post" className="mt-4 text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+                        {featuredPost.title}
+                    </h2>
+                    <p className="mt-4 text-lg leading-8 text-slate-600">{featuredPost.description}</p>
+                    <div className="mt-4 flex flex-col justify-between gap-6 sm:mt-8 sm:flex-row-reverse sm:gap-8 lg:mt-4 lg:flex-col">
+                        <div className="flex">
+                            <a
+                                href={featuredPost.href}
+                                className="text-sm font-semibold leading-6 text-indigo-600"
+                                aria-describedby="featured-post"
+                            >
+                                Continue reading <span aria-hidden="true">&rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                </article>
+                <div className="mx-auto w-full max-w-2xl border-t border-slate-900/10 pt-12 sm:pt-16 lg:mx-0 lg:max-w-none lg:border-t-0 lg:pt-0">
+                    <div className="-my-12 divide-y divide-slate-900/10">
+                        {posts.map((post) => (
+                            <article key={post.id} className="py-12">
+                                <div className="group relative max-w-xl">
+                                    <time dateTime={post.datetime} className="block text-sm leading-6 text-slate-600">
+                                        {post.date}
+                                    </time>
+                                    <h2 className="mt-2 text-lg font-semibold text-slate-900 group-hover:text-slate-600">
+                                        <a href={post.href}>
+                                            <span className="absolute inset-0" />
+                                            {post.title}
+                                        </a>
+                                    </h2>
+                                    <p className="mt-4 text-sm leading-6 text-slate-600">{post.description}</p>
+                                </div>
+                            </article>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/layout/hero.tsx
@@ -0,0 +1,93 @@
+import { LinkedInLogoIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import { getLatestPost } from "@/util/posts";
+
+export default function Hero() {
+    return (
+        <div className="relative isolate overflow-hidden bg-white">
+            <svg
+                className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                aria-hidden="true"
+            >
+                <defs>
+                    <pattern
+                        id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+                        width={200}
+                        height={200}
+                        x="50%"
+                        y={-1}
+                        patternUnits="userSpaceOnUse"
+                    >
+                        <path d="M.5 200V.5H200" fill="none" />
+                    </pattern>
+                </defs>
+                <rect width="100%" height="100%" strokeWidth={0} fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" />
+            </svg>
+            <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+                <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
+                    <div className="mt-24 sm:mt-32 lg:mt-16">
+                        <a href="#" className="inline-flex space-x-6">
+                            <span className="rounded-full bg-slate-600/10 px-3 py-1 text-sm font-semibold leading-6 text-slate-600 ring-1 ring-inset ring-slate-600/10">
+                                Latest update
+                            </span>
+                            <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
+                                <Tooltip>
+                                    <TooltipTrigger asChild>
+                                        <span>{getLatestPost().title}</span>
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <span>{getLatestPost().date.toLocaleDateString("en-AU")}</span>
+                                    </TooltipContent>
+                                </Tooltip>
+                                <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </span>
+                        </a>
+                    </div>
+                    <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                        Mathias Everson
+                    </h1>
+                    <p className="mt-6 text-lg leading-8 text-gray-500 flex flex-row gap-x-2">
+                        <span>🎓</span>
+                        <span>IT grad from <a className="text-gray-600 underline underline-offset-2" target="_blank" href="https://canberra.edu.au/">University of Canberra</a></span>
+                    </p>
+                    <p className="mt-2 text-lg leading-8 text-gray-500 flex flex-row gap-x-2">
+                        <span>👨‍💻</span>
+                        <span>Digital Team Lead at <a className="text-gray-600 underline underline-offset-2" target="_blank" href="https://threesides.com.au/">Threesides Marketing</a></span>
+                    </p>
+                    <p className="mt-2 text-lg leading-8 text-gray-500 flex flex-row gap-x-2">
+                        <span>🌆</span>
+                        <span><a className="text-gray-600 underline underline-offset-2" target="_blank" href="https://canberra.com.au/">Canberra</a> local</span>
+                    </p>
+                    <div className="mt-10 flex items-center gap-x-6">
+                        <a
+                            target="_blank"
+                            href="https://www.linkedin.com/in/mathiaseverson/"
+                            className="rounded-md flex items-center gap-x-2 bg-slate-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
+                        >
+                            <LinkedInLogoIcon className="h-4s w-4s" />
+                            Meet Mathias
+                        </a>
+                        <a href="mailto:mathias@mathiaseverson.com" className="text-sm font-semibold leading-6 text-gray-900">
+                            Get in touch <span aria-hidden="true">→</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                    <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+                        <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                            <Image
+                                src={'/img/mathias-everson.jpg'}
+                                alt="Mathias Everson"
+                                width={1200}
+                                height={800}
+                                className="w-[700px] h-[600px] object-cover rounded-md shadow-2xl ring-1 ring-gray-900/10"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
